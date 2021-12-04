daac38108558cf0e1739c596a561ca8aef0211e9
Hao Li <lihaobhsfer@gmail.com>
2021-12-03 23:56:36 -0500

    [progress bar] adjust based on course solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
+
 const ProgressBar = ({ value, size, showValue }) => {
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
   return (
     <ProgressWrapper
       role="progressbar"
@@ -14,56 +37,54 @@ const ProgressBar = ({ value, size, showValue }) => {
       aria-valuemax="100"
     >
       {showValue && <span>{value}%</span>}
-      <Wrapper size={size}>
-        <Progress value={value} />
+      <Wrapper
+        size={size}
+        style={{
+          "--padding": styles.padding + "px",
+          "--radius": styles.radius + "px",
+        }}
+      >
+        <BarWrapper>
+          <Bar
+            value={value}
+            style={{
+              "--width": value + "%",
+              "--height": styles.height + "px",
+            }}
+          />
+        </BarWrapper>
       </Wrapper>
     </ProgressWrapper>
   );
 };
 
 const ProgressWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 8px;
 `;
 
-const Wrapper = styled.div`
-  width: 100%;
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  background: rgba(128, 128, 128, 0.15);
-
-  ${(props) => {
-    switch (props.size) {
-      case "large":
-        return css`
-          height: 24px;
-          border-radius: 8px;
-          padding: 4px;
-        `;
-      case "medium":
-        return css`
-          height: 12px;
-          border-radius: 4px;
-        `;
-      case "small":
-        return css`
-          height: 8px;
-          border-radius: 4px;
-        `;
-      default:
-        return css`
-          height: 16px;
-        `;
-    }
-  }};
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
 `;
 
-const Progress = styled.div`
-  width: ${(props) => props.value + "%"};
-  background: #4747eb;
-  height: 100%;
-  overflow: hidden;
+const Wrapper = styled.div`
+  width: 100%;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
 `;
 
 export default ProgressBar;
